231599bd6468338b3cddd70e18f19a1e98091731
TechwithAniket <sharmajihastir@gmail.com>
2025-04-14 21:30:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { createRoot } from 'react-dom/client'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Welcome from './Welcome.jsx'
+
+function App() {
+return(
+  <div>
+<h1>WE ARE BACK !!!!!</h1>
+<Welcome/>
+</div>
+)
+}
+export default App
---
+++ src/Welcome.jsx
@@ -0,0 +1,5 @@
+ function Welcome(){
+    return (<h1>Welcome to our home page</h1>)
+}
+
+export default Welcome
